0300412469d22df536d466ac9e1f2916765e265b
premchand13-gif <premechand1301@gmail.com>
2023-01-13 13:14:28 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p=new Product();
+        double x=10;
+        double y=20;
+        System.out.println(p.product(10,20));
+        System.out.println(p.product(10,20,30));
+        System.out.println(p.product(x,y));
 
+
+    }
+}
+class Product{
+    public int product(int x, int y) {
+        return x*y;
+    }
+    public int product(int x, int y, int z) {
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
 }
